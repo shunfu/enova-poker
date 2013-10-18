e6f8d62018f0f75e2d10c7b3f825246919a88759
Sam Fu <shunhua.fu@gmail.com>
2013-10-17 20:19:07 -0500

    Combined logic into poker

--- poker.py
+++ poker.py
@@ -15,11 +15,53 @@ def poker(PLAYER_KEY):
         # Gets JSON in response
         turn_data = game_state(PLAYER_KEY)
         
-        # Logic!!
-        # This logic is boring. But, yours should be more epic!
+        # Logic!
+        # Check if it's your turn
         if turn_data["your_turn"]:
-            # LOGIC GOES HERE
+            card1 = data['hand'][0]
+            card2 = data['hand'][1]
 
+            #deal phase
+            if data["betting_phase"] == "deal":
+                #if the ante is just normal ante then just pay ante
+                if data[]
+                #if the ante is higher and your hand sucks then fold
+                #if ante and your hand is good then pay/raise
+                if data["call_amount"] == :
+                    return_action = {'action_name':"call"}
+
+                #else fold
+                else:
+                    return_action{'action_name':"fold"}
+
+            #flop phase
+            if data["betting_phase"] == "flop":
+                #calcuate the probability of winning the turn
+                #if probability is greater than 50% then bet
+
+                #else fold
+                else:
+                    return_action{'action_name':"fold"}
+            #turn phase
+            if data["betting_phase"] == "turn":
+                #update the probability
+                #if it is still greater than 50% then continue
+                
+                #else fold
+                else:
+                    return_action{'action_name':"fold"}
+
+            #river phase
+            if data["betting_phase"] == "river":
+                #update the final probabilty
+                #if the chance of winning is still greater than 50% then raise
+            
+            #else fold
+            else:
+                return_action{'action_name':"fold"}
+
+""" 
+        This is the provided sample dumb_bot code
         # Is it a betting round, but not the river? Let's always call.
         if  turn_data["betting_phase"] == "deal" or
             turn_data["betting_phase"] == "flop" or
@@ -34,6 +76,7 @@ def poker(PLAYER_KEY):
   
         # Stores all your parameters in a single variable
         my_action = {action_name: action, amount: params}
+"""
     
         # POST a request to the server
         response = player_action(PLAYER_KEY, my_action)
