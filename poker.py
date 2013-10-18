2eeaa1445ec92ae17643524239cf667adc341eee
Sam Fu <shunhua.fu@gmail.com>
2013-10-17 21:02:22 -0500

    Fixed the code. Should work now.

--- poker.py
+++ poker.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import json
 
 def poker(PLAYER_KEY):
     # Infinite Loop
@@ -21,6 +22,7 @@ def poker(PLAYER_KEY):
             card1 = data['hand'][0]
             card2 = data['hand'][1]
             current_money = data['stack']
+            return_action = {}
 
             #deal phase
             if data["betting_phase"] == "deal":
@@ -29,64 +31,47 @@ def poker(PLAYER_KEY):
                 
                 if bet_amount == 0:
                     bet_amount == 20
-                elif bet_amount >= current_money :
+                elif bet_amount >= current_money:
                     bet_amount = current_money
 
-                return_action{'action_name':"bet", 'amount': bet_amount}
+                return_action = {'action_name': "bet", 'amount': bet_amount}
 
             #flop phase
             if data["betting_phase"] == "flop":
-               bet_amount = data['call_amount']
+
+                bet_amount = data['call_amount']
                 
                 if bet_amount == 0:
                     bet_amount == 20
                 elif bet_amount >= current_money :
                     bet_amount = current_money
 
-                return_action{'action_name':"bet", 'amount': bet_amount}
+                return_action = {'action_name': "bet", 'amount': bet_amount}
             #turn phase
             if data["betting_phase"] == "turn":
-               bet_amount = data['call_amount']
+                bet_amount = data['call_amount']
                 
                 if bet_amount == 0:
                     bet_amount == 20
                 elif bet_amount >= current_money :
                     bet_amount = current_money
 
-                return_action{'action_name':"bet", 'amount': bet_amount}
+                return_action = {'action_name':"bet", 'amount': bet_amount}
 
             #river phase
             if data["betting_phase"] == "river":
-               bet_amount = data['call_amount']
+                bet_amount = data['call_amount']
                 
                 if bet_amount == 0:
                     bet_amount == 20
                 elif bet_amount >= current_money :
                     bet_amount = current_money
 
-                return_action{'action_name':"bet", 'amount': bet_amount}
-            
-""" 
-        This is the provided sample dumb_bot code
-        # Is it a betting round, but not the river? Let's always call.
-        if  turn_data["betting_phase"] == "deal" or
-            turn_data["betting_phase"] == "flop" or
-            turn_data["betting_phase"] == "turn":
-            action = "call"
-            params = nil
-                  
-        # Is it the river phase? Always bet 10 more.
-        elif turn_data["betting_phase"] == "river":
-            action = "bet"
-            params = 10
-  
-        # Stores all your parameters in a single variable
-        my_action = {action_name: action, amount: params}
-"""
-    
-        # POST a request to the server
-        response = player_action(PLAYER_KEY, return_action)
+                return_action = {'action_name': "bet", 'amount': bet_amount}
 
+            # POST a request to the server
+            response = player_action(PLAYER_KEY, return_action)
+            
 # GET
 def game_state(key):
     # do a get request to http://nolimitcodeem.com/api/players/:key
@@ -100,12 +85,16 @@ def player_action(key, json_params):
     # do a post request to http://nolimitcodeem.com/api/players/:key/action
     # get a Response object
     # return json
-    r = requests.post('http://nolimitcodeem.com/api/players/{}/action'.format(key))
+    # r = requests.post(url, data=json.dumps(payload), headers=headers)
+    headers = {'Content-type': 'application/json'}
+    r = requests.post('http://nolimitcodeem.com/api/players/{}/action'.format(key), 
+        data=json.dumps(json_params), 
+        headers=headers)
     return r.json()
 
 def main():
     # the key is generated when we register for the tournament
-    poker(60fd7cbe-f9e0-4ebe-a0e3-f554872a312f)
+    poker('60fd7cbe-f9e0-4ebe-a0e3-f554872a312f')
 
 if __name__ == '__main__':
     main()
